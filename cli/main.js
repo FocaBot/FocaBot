292de77a1ab00ba0ea79fc0319d4c4dd0a6a7b8b
TheBITLINK aka BIT <me@thebitlink.com>
2018-03-06 16:39:16 -0400

    1.0.4

--- cli/main.js
+++ cli/main.js
@@ -15,7 +15,7 @@ mkdirp.sync(dataPath)
 console.log(`
        .-.
       :   ;
-       "."               FocaBot v1.0.3 (Elegant Erizo)
+       "."               FocaBot v1.0.4 (Elegant Erizo)
        / \\               by > thebit.link
       /  |
     .'    \\
--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js')
 console.log(`
        .-.
       :   ;
-       "."               FocaBot v1.0.3 (Elegant Erizo)
+       "."               FocaBot v1.0.4 (Elegant Erizo)
        / \\               by > thebit.link
       /  |
     .'    \\
